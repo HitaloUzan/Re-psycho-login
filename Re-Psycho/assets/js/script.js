67b14b4941c3bdd7ad9663ce66f910235a95f44b
HitaloUzan <96554318+HitaloUzan@users.noreply.github.com>
2024-01-13 11:13:33 -0300

    Add files via upload

---
+++ Re-Psycho/assets/js/script.js
@@ -0,0 +1,9 @@
+function logar() {
+
+    var login = document.getElementById('Login').value;
+    var senha = document.getElementById('senha').value;
+
+    if (login == "Admin" && senha == "admin"){
+        alert("Sucesso")
+    }
+}
